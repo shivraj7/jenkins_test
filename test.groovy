afb27f6b86e905095fe29e8deee5eb60731208a8
shivraj7 <patil.shiv2288@gmail.com>
2023-10-16 13:09:48 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,9 +1,24 @@
+Boolean bool = true
+
 pipeline {
   agent any
   stages {
     stage ('Stage 1') {
       steps {
-        echo "hello hello hello"
+        echo "hello1 "
+      }
+    }
+    stage ('Stage 2') {
+      when {
+        expression { bool }
+      }
+      steps {
+        echo "hello2"
+      }
+    }
+    stage ('Stage 3') {
+      steps {
+        echo "hello3"
       }
     }
   }
